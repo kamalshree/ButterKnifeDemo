94cb7224ad628de5bcf1b1280011ba2472a691c1
codesqills <codesqills@gmail.com>
2018-06-03 22:41:00 -0400

    added switch for button click

--- app/src/main/java/com/android/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/android/butterknifedemo/MainActivity.java
@@ -2,8 +2,10 @@ package com.android.butterknifedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import butterknife.BindString;
@@ -27,14 +29,19 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.button_bn)
-    public void buttonClick(){
-        myText=myEditText.getText().toString();
-        myTextView.setText(myText);
+    @OnClick({R.id.button_bn,R.id.change_button_bn})
+    public void buttonClick(View v){
+        switch(v.getId()){
+
+            case R.id.button_bn:
+                myText=myEditText.getText().toString();
+                myTextView.setText(myText);
+                break;
+            case R.id.change_button_bn:
+                myTextView.setAllCaps(true);
+                break;
+
+        }
     }
 
-    @OnClick(R.id.change_button_bn)
-    public void changeButtonClick(){
-        myTextView.setAllCaps(true);
-    }
 }
